ec811cda3712ae74378d4080d7c7ac8cb2167a33
ogheneokeno <ogheneokeno@gmail.com>
2025-08-18 14:46:54 +0100

    fourCheckPositive

---
+++ Java/Task4.java
@@ -0,0 +1,32 @@
+//1.write app that asks user to enter two integers,
+//obtains them from the user and prints square of each, 
+//the sum of their squares, and 
+//difference of the squares first number squared minus second number squared
+
+//2. write app that inputs three integers from user and displays sum ,
+//average ,product and largest
+
+//3. write app that reads an integer and determines and prints whether its divisible by 3 or not. hint 
+// use remainder opertor. a number is divisible by 3 if it divided by 3 with remainder of 0
+//4.write program that checks if a number is positive or negative
+
+
+public class checkPositive{
+
+public static void main(String[]args){
+isPositive(5);
+isPositive(-6);
+isPositive(0);
+
+public static boolean isPositive(int number){
+if (number>0){
+
+System.out.println("number is positive:")
+elseif(number<0){
+System.out.println("Number is negative");
+else:
+System.out.println("Number is equals to zero");
+return(number);
+}
+}
+}
