0c5033af539d0a552d3b42f86b1a654e83f6278e
BluPearl-J <ogheneokeno@gmail.com>
2025-08-18 15:51:08 +0100

    Create Fourteen.java

---
+++ Java/Fourteen.java
@@ -0,0 +1,41 @@
+//1.write app that asks user to enter two integers,
+//obtains them from the user and prints square of each, 
+//the sum of their squares, and 
+//difference of the squares first number squared minus second number squared
+
+//2. write app that inputs three integers from user and displays sum ,
+//average ,product and largest
+
+//3. write app that reads an integer and determines and prints whether its divisible by 3 or not. hint 
+// use remainder opertor. a number is divisible by 3 if it divided by 3 with remainder of 0
+//4.write program that checks if a number is positive or negative
+//5.print numbers from 1-10
+//6 find largest number in an array
+//7 find smallest number in an array
+//8 find sum of all elements in an array
+//9 print element in an array in reverse order
+//10find max and minimum in ana array usinga loop
+//11 write program to sort array without built in functions
+
+//12 writwe program to sort array without creating new variable
+//13 find second largest in an array
+//14 find factorial of a number
+//15 find sum of first natural 100 natural numbers
+//16 check if number ends with digit 5
+
+
+public class Fourteen{
+
+
+public static void main(String[]args){
+
+int factorial=0;
+int number;
+for(int index=1;index<=number; index--){
+factorial+=(number*index);
+System.out.println("factorial is:", factorial);
+
+
+}
+}
+}
