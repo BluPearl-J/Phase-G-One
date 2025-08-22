ef035bfdfb7dc99b3dfc027bd28dd4c3ca9be066
BluPearl-J <ogheneokeno@gmail.com>
2025-08-22 08:15:37 +0100

    Create GroceryManagernotcollectingInput.py

---
+++ python/dayfour.py/GroceryManagernotcollectingInput.py
@@ -0,0 +1,18 @@
+grocery_list = []
+
+def add_item(item):
+    grocery_list.append(item)
+    return f"{item} added."
+
+def remove_item(item):
+    if item in grocery_list:
+        grocery_list.remove(item)
+        return f"{item} removed from list."
+    else:
+        return f"{item} not found in  list."
+
+def show_items():
+    if not grocery_list:
+        return "Grocery list is empty."
+    else:
+        return [f"{idx}. {item}" for idx, item in enumerate(grocery_list, start=1)]
