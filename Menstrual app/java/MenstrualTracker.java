a1e75df00b7389db9980186f8b98dd4733979f63
ogheneokeno <ogheneokeno@gmail.com>
2025-08-24 06:03:11 +0100

    adding 32days logic and negative numbers reject

--- Menstrual app/java/MenstrualTracker.java
+++ Menstrual app/java/MenstrualTracker.java
@@ -11,9 +11,15 @@ public static int getFlowDates(int startDatePeriod,int EndDatePeriod){
 System.out.println("Enter startDate of last most recent period:");
 Scanner input=new Scanner(System.in);
 int periodFirstDate=input.nextInt();
+if(periodFirstDate>31||periodFirstDate<0){
+throw new IllegalArgumentException("ERROR;DATE OF THE MONTH CANNOT BE NEGATIVE OR GREATER THAN 31!!!ENTER ANY INTEGER BETWEEN 1 AND 31 ONLY");
+
+}
 System.out.println("Enter lastDate of last most recent period:");
 int periodLastDate=input.nextInt();
-
+if(periodLastDate>31||periodLastDate<0){
+throw new IllegalArgumentException("ERROR;DATE OF THE MONTH CANNOT BE NEGATIVE OR GREATER THAN 31!!!ENTER ANY INTEGER BETWEEN 1 AND 31 ONLY");
+}
 int bleedingDays=(periodLastDate-periodFirstDate);
 if(bleedingDays<=2||bleedingDays<=7){
 System.out.println("Congratulations!You have:" + bleedingDays+" " +"menstrualFlowLength."+" " +" "+ "it is perfectly Normal");
@@ -26,5 +32,4 @@ return bleedingDays;
 
 //cycleDuration;
 }
-
 
